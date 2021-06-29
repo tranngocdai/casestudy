a7c258a8450f886609d3309572eab55da066be84
tranngocdai <85327489+tranngocdai@users.noreply.github.com>
2021-06-29 14:00:50 +0700

    game

---
+++ config.js
@@ -0,0 +1,4 @@
+const GRID_SIZE = 15;
+const WITDH = 600;
+const HEIGHT = 600;
+const SNAKE_SPEED = 8;
---
+++ snake.js
@@ -0,0 +1,42 @@
+class Snake {
+    constructor(){
+        this.vel = createVector(0,0);
+        this.head = createVector(0,0);
+        this.length = 5;
+        this.body = [];
+        this.isDead = false;
+    }
+    show() {
+        noStroke();
+        // Draw snake head
+        fill(255);
+        rect(this.head.x, this.head.y, GRID_SIZE, GRID_SIZE);
+        // Draw snake body
+        fill(155);
+        for(let vector of this.body)
+        {
+            rect(vector.x, vector.y, GRID_SIZE, GRID_SIZE);
+        }
+    }
+    update(){
+        this.body.push(createVector(this.head.x, this.head.y));
+
+        this.head.x += this.vel.x * GRID_SIZE;
+        this.head.y += this.vel.y * GRID_SIZE;
+
+        this.head.x = (this.head.x + WITDH) % WITDH;
+        this.head.y = (this.head.y + HEIGHT) % HEIGHT;
+        if(this.length < this.body.length)
+        {
+            this.body.shift();
+        }
+        for(let vector of this.body)
+        {
+            if(vector.x == this.head.x && vector.y == this.head.y)
+            {
+                this.isDead = true;
+            }
+        }
+    }
+
+}
